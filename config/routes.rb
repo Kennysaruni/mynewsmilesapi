8600943440df34fdc23b74b136e993acb983d643
Kennysaruni <kenny.saruni@student.moringaschool.com>
2023-04-02 20:10:47 +0300

    Added Post model + changed index/create methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  resources :posts
   # resources :users
   get '/', to: 'users#index'
   post '/users', to: 'users#create'
+  ##USERS
+  get '/posts', to: 'posts#index'
+  post '/posts', to: 'posts#create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
